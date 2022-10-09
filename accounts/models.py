8f7e2ad40e6a298c460cbf5fb9616198e771f282
walexhenrique <walex999067@gmail.com>
2022-10-09 00:29:11 -0300

    Add model profile

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,10 @@
+
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    slug = models.SlugField()
+    bio = models.CharField(max_length=120, null=True, blank=True)
+    image = models.ImageField(update_to='accounts/covers/%Y/%m/%d/', blank=True, default='')

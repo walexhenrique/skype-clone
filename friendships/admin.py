952f156c17eed03aad4c99d352f2a2139d98d2cb
walexhenrique <walex999067@gmail.com>
2022-10-10 21:46:03 -0300

    Create model friend and initial config app friendships

--- friendships/admin.py
+++ friendships/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Friend
+
+
+@admin.register(Friend)
+class FriendAdmin(admin.ModelAdmin):
+    ...
--- friendships/models.py
+++ friendships/models.py
@@ -1,3 +1,24 @@
+from accounts.models import Profile
 from django.db import models
 
-# Create your models here.
+
+class Friend(models.Model):
+
+    ACCEPTED = 'A'
+    PENDING = 'P'
+    DENIED = 'D'
+
+    STATUS_CHOICES = [
+        (ACCEPTED, 'Accepted'),
+        (PENDING, 'Pending'),
+        (DENIED, 'Denied'),
+    ]
+
+
+    id_requester = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='requester')
+    id_receiver = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='receiver')
+    request_date = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=1, choices=STATUS_CHOICES, default=PENDING)
+    
+

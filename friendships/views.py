c9de95604890382694b77e421321ff16932ba12d
walexhenrique <walex999067@gmail.com>
2022-10-11 00:49:52 -0300

    config Get friends show

--- friendships/views.py
+++ friendships/views.py
@@ -3,8 +3,15 @@ from django.shortcuts import render
 from django.utils.decorators import method_decorator
 from django.views import View
 
+from .models import Friend
+
 
 @method_decorator(login_required, 'dispatch')
 class IndexView(View):
     def get(self, *args, **kwargs):
-        return render(self.request, 'friendships/index.html')
+        user = self.request.user
+        friends_1 = Friend.objects.filter(status='A', id_requester__user=user).order_by('-id')
+        friends_2 = Friend.objects.filter(status='A', id_receiver__user=user).order_by('-id')
+
+        
+        return render(self.request, 'friendships/index.html', {'friends_add': friends_1, 'friends_received': friends_2})
